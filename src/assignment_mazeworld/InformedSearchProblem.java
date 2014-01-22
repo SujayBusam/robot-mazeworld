31dc7a731d236a043d22c2307899585b6b9a9a19
Sujay Busam <sujay.busam@gmail.com>
2014-01-21 21:33:42 -0500

    Initial commit

---
+++ src/assignment_mazeworld/InformedSearchProblem.java
@@ -0,0 +1,23 @@
+package assignment_mazeworld;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class InformedSearchProblem extends SearchProblem {
+	
+	public List<SearchNode> astarSearch() {
+		
+			resetStats();
+			
+			// Initialize fringe / frontier
+			PriorityQueue<SearchNode> pq = new PriorityQueue<SearchNode>();
+			
+			// Initialize explored set (hashmap)
+			HashMap<SearchNode> visited = new HashMap<SearchNode>();
+			
+		}
+	
+	
+}
