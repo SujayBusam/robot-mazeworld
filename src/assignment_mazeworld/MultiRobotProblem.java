9808918ddc60b0de5b5d7bb40957c1281344d74c
Sujay Busam <sujay.busam@gmail.com>
2014-01-22 18:08:38 -0500

    Multi robot complete. Modifying driver to work with class

--- src/assignment_mazeworld/MultiRobotProblem.java
+++ src/assignment_mazeworld/MultiRobotProblem.java
@@ -1,6 +1,7 @@
 package assignment_mazeworld;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import assignment_mazeworld.SearchProblem.SearchNode;
 import assignment_mazeworld.SimpleMazeProblem.SimpleMazeNode;
 
@@ -9,17 +10,17 @@ public class MultiRobotProblem extends InformedSearchProblem {
 	// Five possible actions (modified Maze.java)
 	private static int actions[][] = {Maze.NORTH, Maze.EAST, Maze.SOUTH, Maze.WEST, Maze.PASS};
 
-	private int xStart, yStart, xGoal, yGoal, numBots;
+	private int numBots;
+
+	private int[][] goalState;
 
 	private Maze maze;
 
-	public MultiRobotProblem(Maze m, int sx, int sy, int gx, int gy, int k) {
-		startNode = new MultiRobotNode(sx, sy, 0, 0);
-		xStart = sx;
-		yStart = sy;
-		xGoal = gx;
-		yGoal = gy;
-		numBots = k;
+	public MultiRobotProblem(Maze m, int[][] start, int[][] goal) {
+
+		startNode = new MultiRobotNode(start, 0, 0);
+		goalState = goal;
+		numBots = start.length;
 
 		maze = m;		
 	}
@@ -30,8 +31,8 @@ public class MultiRobotProblem extends InformedSearchProblem {
 		// Keep track of parent for backchaining
 		private SearchNode parent;
 
-		// State. Keeps track of location of all robots in maze and whose turn it is
-		protected int[][] state;
+		// State. Keeps track of location of all robots in maze
+		protected int[][] currentState;
 
 		// Which robot has the turn to move
 		private int turn;
@@ -39,76 +40,79 @@ public class MultiRobotProblem extends InformedSearchProblem {
 		// How far current node is from start
 		private double cost;
 
-		// Constructor for initial start node
-		public MultiRobotNode(double c, int t) {
+		// Constructor
+		public MultiRobotNode(int[][] start, double c, int t) {
 			cost = c;
 
-			// Initial state: bots all at start node
-			state = new int[numBots][2];
-			for (int i = 0; i < numBots; i++) {
-				this.state[i][0] = xStart;
-				this.state[i][1] = yStart;
-			}
-
-			// And first bot's turn to move (bot 0)
-			turn = t;
-		}
-
-		// Constructor for all other successor nodes
-		public MultiRobotNode(int x, int y, double c, int t, int[][] predState) {
-			cost = c;
-
-			// Copy state from predecessor node
-			this.state = copyState(predState);
-
-			// Modify the state so the bot that just moved has its location updated
-			int predBot = (t-1) % numBots;
-			this.state[predBot][0] = x;
-			this.state[predBot][1] = y;
-
-			// Turn is updated
-			turn = t;
+			// Initialize state
+			this.currentState = start;
 		}
 
 		// Get the valid successors of the current node
 		public ArrayList<SearchNode> getSuccessors() {
 
-			ArrayList<SearchNode> successors = new ArrayList<SearchNode>();
-						
-			// Run through every bot, starting with the one to move
-			for (int i = turn; i < numBots; i = (i+1) % numBots) {
-				
-				// Run through all possible actions for current bot
-				for (int[] action: actions) {
-					int xNew = state[i][0] + action[0];
-					int yNew = state[i][1] + action[1]; 
+			ArrayList<SearchNode> successorsList = new ArrayList<SearchNode>();
 
-					//System.out.println("testing successor " + xNew + " " + yNew);
 
-					if(maze.isLegal(xNew, yNew)) {
-						//System.out.println("legal successor found " + " " + xNew + " " + yNew);
-						
-					}
+			// Run through all possible actions for bot whose turn it is to move
+			for (int[] action: actions) {
+				int xNew = currentState[turn][0] + action[0];
+				int yNew = currentState[turn][1] + action[1]; 
 
+				//System.out.println("testing successor " + xNew + " " + yNew);
+
+				if(maze.isLegal(xNew, yNew) && !isOccupied(xNew, yNew)) {
+					//System.out.println("legal successor found " + " " + xNew + " " + yNew);
+
+					// Make a copy of the current state
+					int[][] newState = copyState(this.currentState);
+
+					// Modify current state appropriately
+					newState[turn][0] = xNew;
+					newState[turn][1] = yNew;
+
+					// Create successor node with this new state
+					MultiRobotNode successor = new MultiRobotNode(newState,
+							getCost() + 1.0, (turn + 1) % numBots);
+
+					// Add successor to list
+					successorsList.add(successor);
+				}
+			}
+			return successorsList;
+		}
+
+		// Helper method to see if x,y space is already occupied by a bot
+		private boolean isOccupied(int x, int y) {
+
+			// Run through x,y of all bots (excluding current one) to check for collisions
+			for (int i = (turn + 1); i < (turn % numBots); i = (i + 1) % numBots) {
+
+				// Check if this bot's position matches with the checked bot's position
+				int currentBotX = currentState[i][0];
+				int currentBotY = currentState[i][1];
+
+				if (x == currentBotX && y == currentBotY) {
+					return true;
 				}
 			}
 
-			return successors;
-
+			// No collisions with other bots if for loop exited
+			return false;
 		}
 
 		// Helper method to copy predecessor node's state array
-		private int[][] copyState(int[][] predState) {
+		private int[][] copyState(int[][] oldState) {
 			// Copy to be returned
-			int[][] newArray = new int[numBots][3];
+			int[][] newState = new int[numBots][2];
 
-			for (int i = 0; i < predState.length; i++) {
-				for (int j = 0; j < predState[j].length; j++) {
-					newArray[i][j] = predState[i][j];
+			for (int i = 0; i < oldState.length; i++) {
+				for (int j = 0; j < oldState[j].length; j++) {
+					newState[i][j] = oldState[i][j];
 				}
 			}
 
-			return newArray;
+			return newState;
 		}
 
 		// Get and set the parent node for backchaining
@@ -118,6 +122,89 @@ public class MultiRobotProblem extends InformedSearchProblem {
 
 		public void setParent(SearchNode parent) {
 			this.parent = parent;
+		}
+
+
+		@Override
+		public boolean goalTest() {
+			return this.currentState.equals(goalState);
+		}
+
+
+		// an equality test is required so that visited sets in searches
+		// can check for containment of states
+		@Override
+		public boolean equals(Object other) {
+
+			for (int i = 0; i < numBots; i++) {
+				if (!Arrays.equals(this.currentState[i], ((MultiRobotNode) other).currentState[i])) {
+					return false;
+				}
+			}
+
+			// Dropped out of loop. All arrays are equal
+			return true;
+		}
+
+//		@Override
+//		public int hashCode() {
+//			return state[0] * 100 + state[1]; 
+//		}
+
+		//		@Override
+		//		public String toString() {
+		//			return new String("Maze state " + state[0] + ", " + state[1] + " "
+		//					+ " depth " + getCost());
+		//		}
+
+		@Override
+		public double getCost() {
+			return cost;
+		}
+
+
+		@Override
+		public double heuristic() {
+			// Manhattan distance taking into account all robots
+
+			// Get sum of all x values of robots in current state
+			double totalCurrentX = 0;
+			for (int i = 0; i < numBots; i++) {
+				totalCurrentX += this.currentState[i][0];
+			}
+
+			// Get sum of all y values of robots in current state
+			double totalCurrentY = 0;
+			for (int i = 0; i < numBots; i++) {
+				totalCurrentY += this.currentState[i][1];
+			}
+
+			// Get sum of all x values of robots in goal state
+			double totalGoalX = 0;
+			for (int i = 0; i < numBots; i++) {
+				totalGoalX += goalState[i][0];
+			}
+
+			// Get sum of all y values of robots in goal state
+			double totalGoalY = 0;
+			for (int i = 0; i < numBots; i++) {
+				totalGoalY += goalState[i][1];
+			}
+			
+			// Get the difference and return
+			double dx = totalGoalX - totalCurrentX;
+			double dy = totalGoalY - totalCurrentY;
+			return Math.abs(dx) + Math.abs(dy);
+		}
+
+		@Override
+		public int compareTo(SearchNode o) {
+			return (int) Math.signum(priority() - o.priority());
+		}
+
+		@Override
+		public double priority() {
+			return heuristic() + getCost();
 		}
 	}
 }
